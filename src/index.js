c18180176b8ebeb0e9bc47f32e239565a27bf78f
Leonardo Sampaio <leyrakos.l@gmail.com>
2022-03-21 17:58:23 -0300

    space invaders v1.1 (alien bullets bug fixed)

--- src/index.js
+++ src/index.js
@@ -3,27 +3,31 @@ import { Bullet } from "./Bullet.js";
 import { Alien } from "./Alien.js";
 import { Life } from "./Life.js";
 
+
 const ship = new Ship();
 const shipBullets = [];
 const aliens = [];
 const alienBullets = [];
 const lives = []
-var shootingAliens = [40, 41, 42, 43, 44, 45, 46, 47, 48, 49]
 var alienCount = 50
+var score = 0
 const key = {
     a: false,
     d: false,
     [" "]: false
 }
 
-for(var j=100; j < 350; j += 50){
-    for (var i=433; i < 923; i += 50){
-        aliens.push(new Alien({x: i, y: j}));
+
+for (var i=433; i < 923; i += 50){
+    aliens.push([])
+    for(var j=100; j < 350; j += 50){
+        aliens[aliens.length - 1].push(new Alien({x: i, y: j}));
     }
 }
 for(var i=0; i < ship.lives; i += 1){
     lives.push(new Life)
 }
+
 
 document.addEventListener("keydown", (event) => {
     key[event.key] = true
@@ -31,28 +35,32 @@ document.addEventListener("keydown", (event) => {
 document.addEventListener("keyup", (event) => {
     key[event.key] = false
 })
-console.log(ship.y + 30)
+
+
 var alienShots = setInterval(() => {
     if (ship.lives -1 < 0){
         clearInterval(alienShots)
     }
     if(alienCount > 0){
-        const i = Math.floor(Math.random() * shootingAliens.length);
-        var x = aliens[shootingAliens[i]].x
-        var y = aliens[shootingAliens[i]].y
+        const i = Math.floor(Math.random() * aliens.length);
+        const j = aliens[i].length - 1
+        var x = aliens[i][j].x
+        var y = aliens[i][j].y
         alienBullets.push(new Bullet({alien: true, x: x, y: y},))
     }else{
         clearInterval(alienShots);
     } 
-},1000)
+}, 300)
 
 var game = setInterval(() => {
     ship.moveLeft(key["a"]);
     ship.moveRight(key["d"]);
-    aliens.forEach((alien) => {
-        if(alien !== 0){
-            alien.move()
-        }
+    aliens.forEach((alienColumn) => {
+        alienColumn.forEach((alien) =>{
+            if(alien !== 0){
+                alien.move()
+            }
+        })
     })
     
     alienBullets.forEach((alienBullet) => {
@@ -63,26 +71,21 @@ var game = setInterval(() => {
     })
 
     shipBullets.forEach((shipBullet) => {
-        aliens.forEach((alien) => {
-            if (shipBullet.y > alien.y && shipBullet.y < (alien.y + 30) &&
-                shipBullet.x > (alien.x - 10) && shipBullet.x < (alien.x + 40)){
-                    var deletedAlien = aliens.indexOf(alien)
-                    alien.remove()
-                    aliens[deletedAlien] = 0
-                    shipBullet.remove()
-                    shipBullets.splice(shipBullets.indexOf(shipBullet), 1)
-                    alienCount -= 1;
-                    if (shootingAliens.includes(deletedAlien)){
-                        var i = shootingAliens.indexOf(deletedAlien);
-                        var newShootingAlien = deletedAlien - 10
-                        console.log(newShootingAlien)
-                        if (newShootingAlien > 0){
-                            shootingAliens[i] = newShootingAlien
-                        }else{
-                            shootingAliens.splice(i, 1)
-                        }
+        aliens.forEach((alienColumn) => {
+            alienColumn.forEach((alien) => {
+                if (shipBullet.y > alien.y && shipBullet.y < (alien.y + 30) &&
+                    shipBullet.x > (alien.x - 10) && shipBullet.x < (alien.x + 40)){
+                        var alienIndex = alienColumn.indexOf(alien)
+                        var shipBulletIndex = shipBullets.indexOf(shipBullet)
+                        alien.remove()
+                        alienColumn.splice(alienIndex, 1)
+                        shipBullet.remove()
+                        shipBullets.splice(shipBulletIndex, 1)
+                        alienCount -= 1;
+                        score += 200
+                        $(".score").html("Score: " + score) 
                     }
-                }
+            })
         })
     })
     alienBullets.forEach((alienBullet) => {
@@ -105,17 +108,27 @@ var game = setInterval(() => {
     })
 
     if (key[" "]){
-        if ((shipBullets.length === 0) || shipBullets[shipBullets.length - 1].y < 325){
+        if ((shipBullets.length === 0) || shipBullets[shipBullets.length - 1].y < 375){
             shipBullets.push(new Bullet({x: ship.x + 35, y: ship.y - 15}))
         }
     }
     if ((shipBullets.length > 0) && shipBullets[0].y < 0){
         shipBullets[0].remove();
         shipBullets.shift();
-        
+        if (score > 0){
+            score -= 100
+            $(".score").html("Score: " + score)
+        }
     }
     if ((alienBullets.length > 0) && alienBullets[0].y > 635){
         alienBullets[0].remove();
         alienBullets.shift();
+    }
+    if (alienCount === 0){
+        var gameOver = document.createElement("img")
+        gameOver.src = "../images/gameOver.png"
+        gameOver.className = "gameOverScreen"
+        document.body.appendChild(gameOver)
+        clearInterval(game)
     }
 }, 20)
